f947e2d1d6290a9e63208b11b984329f674fb93d
Suraj Patra <patrasuraj2000@gmail.com>
2023-03-03 11:16:24 +0530

    Added loading feature

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-import { quotes } from "./quotes.js";
+// import { quotes } from "./quotes.js";    // For local file
+let quotes = []; // For api
 
 let quoteContainer = document.getElementById('quote-container');
 let quoteText = document.getElementById('quote');
 let authorText = document.getElementById('author');
 let twitterBtn = document.getElementById('twitter');
 let newQuoteBtn = document.getElementById('new-quote');
+let loader = document.getElementById('loader');
+
+function loadingStart() {
+    loader.classList.remove("hide");
+    quoteContainer.classList.add("hide");
+}
+function loadingEnd() {
+    loader.classList.add("hide")
+    quoteContainer.classList.remove("hide");
+}
 
 
 // Generating new quote :
 function newQuote() {   
+    loadingStart();
+
     let random = Math.floor(Math.random()*quotes.length);
     let {author, text} =  quotes[random];
     (text.length > 60) && (quoteContainer.classList.add('long-quote'));
     quoteText.textContent = text;
     authorText.textContent = author? author:'Unknown';
+
+    loadingEnd();
 }
 // Tweet quote :
 function tweetQuote() {
@@ -21,24 +36,24 @@ function tweetQuote() {
     window.open(twitterUrl, '_blank');
 }
 
-newQuote();
 newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
 
 
-// // Get API quotes
-// let quotes = [];
-// async function getQuote() {
-//     const apiUrl = 'https://type.fit/api/quotes';
-//     try {
-//         const response = await fetch(apiUrl);
-//         quotes = await response.json();
-//         newQuote();
-//     } catch(err) {
-//         console.log(err);
-//     }
-// }
+// Get API quotes
+async function getQuote() {
+    loadingStart();
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        quotes = await response.json();
+        newQuote();
+    } catch(err) {
+        console.log(err);
+    }
+}
 
-// // on load - start here
-// getQuote();
+// on load - start here
+getQuote(); // For api
+// newQuote();  // For local file
